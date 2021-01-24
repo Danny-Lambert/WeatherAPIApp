d55f6e082ce335dd759b1568dba03d364ae23fd7
Danny Lambert <dlambert150293@gmail.com>
2021-01-24 15:14:42 +0000

    added in temp, feels like and wind speed- still styling to do

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Leeds&appid=bc52d396a55a
     })
     .then ((jsonResponse) => {
         console.log(jsonResponse)
-        document.querySelector(".currentWeather").innerHTML = jsonResponse.main.temp;
+        document.querySelector(".temperature").innerHTML = jsonResponse.main.temp;
+        document.querySelector(".location").innerHTML = jsonResponse.name;
+        document.querySelector(".feelsLike").innerHTML = jsonResponse.main.feels_like;
+        document.querySelector(".windSpeed").innerHTML = jsonResponse.wind.speed;
+
     })
     .catch((error) => {
         print (error)
