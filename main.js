2756e78a96bed374dc11ee1c2fe2bc36e8bfdc7d
Danny Lambert <dlambert150293@gmail.com>
2021-01-25 22:00:48 +0000

    api key change

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ console.log("working");
 
 
 
-fetch("http://api.openweathermap.org/data/2.5/weather?q=Leeds&appid=bc52d396a55ab51ee55d98516702facc&units=metric")
     .then((response) => {
         return response.json()
     })
