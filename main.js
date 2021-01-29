e145d8d31dc7fd6aada381c9d092dbd30e776f6b
Danny Lambert <dlambert150293@gmail.com>
2021-01-29 15:06:40 +0000

    changing to mobile first approach

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 console.log("working");
 
 
-
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Leeds&appid=bc52d396a55ab51ee55d98516702facc&units=metric")
     .then((response) => {
         return response.json()
     })
     .then ((jsonResponse) => {
         console.log(jsonResponse)
-        document.querySelector(".temperature").innerHTML = jsonResponse.main.temp;
-        document.querySelector(".location").innerHTML = jsonResponse.name;
-        document.querySelector(".feelsLike").innerHTML = jsonResponse.main.feels_like;
+        document.querySelector(".temperatureAPI").innerHTML = jsonResponse.main.temp;
+        document.querySelector(".locationAPI").innerHTML = jsonResponse.name;
+        document.querySelector(".feelsLikeAPI").innerHTML = jsonResponse.main.feels_like;
         document.querySelector(".windSpeed").innerHTML = jsonResponse.wind.speed;
 
     })
