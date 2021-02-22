ce9a5f02ef76075ea4c0be6bb94bff7a7a825cc7
Danny Lambert <dlambert150293@gmail.com>
2021-02-22 17:57:24 +0000

    new city added to weather app

--- main.js
+++ main.js
@@ -30,3 +30,16 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Leeds&appid=bc52d396a55a
         document.querySelector(".BarcawindSpeedAPI").innerHTML = jsonResponse.wind.speed;
 
     })
+
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=berlin&appid=bc52d396a55ab51ee55d98516702facc&units=metric")
+    .then((response) => {
+        return response.json()
+    })
+    .then ((jsonResponse) => {
+        console.log(jsonResponse)
+        document.querySelector(".BerlintemperatureAPI").innerHTML = jsonResponse.main.temp;
+        document.querySelector(".BerlinlocationAPI").innerHTML = jsonResponse.name;
+        document.querySelector(".BerlinfeelsLikeAPI").innerHTML = jsonResponse.main.feels_like;
+        document.querySelector(".BerlinwindSpeedAPI").innerHTML = jsonResponse.wind.speed;
+
+    })
