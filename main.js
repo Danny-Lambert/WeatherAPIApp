b457309f50aa00f9062ea3107c073a696653330e
Danny Lambert <dlambert150293@gmail.com>
2021-03-11 17:19:37 +0000

    changed order of js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ console.log("working");
 
 const button = document.querySelector('.button')
 const inputValue = document.querySelector('.inputValue')
-const name = document.querySelector('.name');
-const desc = document.querySelector('.desc');
-const temperatureAPI = document.querySelector('.temperatureAPI');
+// const name = document.querySelector('.name');
+// const desc = document.querySelector('.desc');
+// const temperatureAPI = document.querySelector('.temperatureAPI');
 
 
 button.addEventListener('click', function(){
@@ -18,8 +18,12 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&app
 
     document.querySelector(".temperatureAPI").innerHTML = jsonResponse.main.temp;
     document.querySelector(".locationAPI").innerHTML = jsonResponse.name;
+    // weather.iconId = jsonResponse.weather[0].icon;    
+    document.querySelector(".icon").innerHTML = jsonResponse.weather[0].icon;
+
+    // <img src='http://openweathermap.org/img/wn/"+jsonResponse.weather[0].icon+".png' class="icon"></img>;
     document.querySelector(".feelsLikeAPI").innerHTML = jsonResponse.main.feels_like;
-    document.querySelector(".windSpeedAPI").innerHTML = jsonResponse.wind.speed;
+    // document.querySelector(".windSpeedAPI").innerHTML = jsonResponse.wind.speed;
 })
     
 
